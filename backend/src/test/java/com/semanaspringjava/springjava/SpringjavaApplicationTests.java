9db8fdc44c88aea314210f0f8307cb80ad6e316a
Gabrielle Simões <gabrielle.simoes@hotmail.com>
2022-11-15 15:33:52 -0300

    projeto criado

---
+++ backend/src/test/java/com/semanaspringjava/springjava/SpringjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semanaspringjava.springjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
