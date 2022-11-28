90fdc5399ea5b6ad1ff0988ef2e7469a308791ca
Gabrielle Simões <gabrielle.simoes@hotmail.com>
2022-11-28 15:31:40 -0300

    enviando notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification_icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
+/*props - como se fossem parâmetros que um 
+componente do react pode receber*/ 
+type Props = {
+  saleId: number;
+}
+
+/*função que faz a chamada da api, chama o endpoint que manda o sms*/
+function handleClick(id: number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("SUCESSO");
+  })
+}
+
+function NotificationButton( {saleId} : Props ) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     )
