c97b39ef3221a5d4f85c3dba4ba2f697e9867d8b
Gabrielle Simões <gabrielle.simoes@hotmail.com>
2022-11-29 17:22:12 -0300

    mensagem de confirmação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Header from "./components/NotificationButton/Header";
 import SalesCard from "./components/SalesCard";
+
 
 function App() {
   return (
     <>
+
+    <ToastContainer/>
+
     <Header></Header>
     <main>
       <section id="sales">
